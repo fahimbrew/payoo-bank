ccf1a0977509a9b2baed6ef436fdae5ffdfbdf1b
fahimbrew <iftiaqhossain@gmail.com>
2024-11-30 19:32:18 +0600

    initialized

---
+++ js/login.js
@@ -0,0 +1,7 @@
+// console.log("can you see me?")
+document.getElementById('btn-login').addEventListener('click', function (event) {
+    event.preventDefault();
+    // console.log('clicked')
+    const phoneNumber = document.getElementById('phone-number');
+    console.log(phoneNumber);
+})
