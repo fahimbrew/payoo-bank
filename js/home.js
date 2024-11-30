83b67740e216a378ce4ec49236d4d57cffeffe5e
fahimbrew <iftiaqhossain@gmail.com>
2024-12-01 00:19:13 +0600

    add money feature has been added

--- js/home.js
+++ js/home.js
@@ -3,6 +3,20 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     event.preventDefault();
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('pin-input').value;
-    console.log(addMoneyInput, pinNumberInput);
+    // console.log(addMoneyInput, pinNumberInput);
+    if (pinNumberInput === '1234') {
+        // console.log('money is added to your account');
+        const balanceDiv = document.getElementById('account-balance').innerText;
+        let balanceNumber = Number(balanceDiv);
+        const amountNumber = Number(addMoneyInput);
+        const newBalance = balanceNumber + amountNumber;
+        document.getElementById('account-balance').innerText = newBalance;
+
+
+
+    }
+    else {
+        alert('Failed to add money! please try again later');
+    }
 
 })
